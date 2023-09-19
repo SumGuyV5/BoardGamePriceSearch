fd79b60edccd4a0486e42d13d650650b855327e6
Richard Allen <richard2891@gmail.com>
2023-09-19 00:36:25 -0400

    update site so that you can check which site you wish to search.

--- Modules/GameSearch.py
+++ Modules/GameSearch.py
@@ -8,11 +8,37 @@ from Modules.LvlupGames import LvlupGames
 from bs4 import BeautifulSoup
 
 
-def search_results(search_for):
+def search_results(search_for, sites):
     start_time = time.time()
     soup = BeautifulSoup('<div class="grid-container"></div>', "html.parser")
-    stores = [BoardGameBliss(search_for), Games401(search_for), MeepleMart(search_for), LegendsWarehouse(search_for),
-              WoodForSheep(search_for), LvlupGames(search_for)]
+    #soup2 = BeautifulSoup('<div><br/></div>', "html.parser")
+    #label = soup2.new_tag('label', attrs={'class': 'checkbox'})
+    #input = label.new_tag('input', attrs={'type': 'checkbox', 'name': 'sites',
+    #                            'value': '401games', 'checked': 'checked'})
+    #stores = [BoardGameBliss(search_for), Games401(search_for), MeepleMart(search_for), LegendsWarehouse(search_for),
+    #          WoodForSheep(search_for), LvlupGames(search_for)]
+    stores = []
+
+    for site in sites:
+        add = None
+        if site == "boardgamebliss":
+            add = BoardGameBliss(search_for)
+        elif site == "401games":
+            add = Games401(search_for)
+        elif site == "meeplemart":
+            add = MeepleMart(search_for)
+        elif site == "legendswarehouse":
+            add = LegendsWarehouse(search_for)
+        elif site == "woodforsheep":
+            add = WoodForSheep(search_for)
+        elif site == "lvlupgames":
+            add = LvlupGames(search_for)
+        else:
+            print("What the ?")
+
+        if add is not None:
+            stores.append(add)
+
     for store in stores:
         store.search()
     for store in stores:
--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, session, render_template, request
 
 from Modules.GameSearch import search_results
 
 app = Flask(__name__)
-
+app.secret_key = 'any random string'
 
 @app.route('/search', methods=['POST'])
 def search():
     if request.method == 'POST':
         search_name = request.form["search_name"]
-        html = search_results(search_name)
+        sites = request.form.getlist("sites")
+        session['sites'] = sites
+        html = search_results(search_name, sites)
         return render_template('index.html', text=html)
     return render_template('index.html')
 
